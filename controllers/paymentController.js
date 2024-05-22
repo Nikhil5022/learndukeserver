bd442593eac2f82a9136000fc1ee633cf98b2227
Gubbala Sai Nikhil <gsn.agc5022@gmail.com>
2024-05-22 15:16:55 +0530

    test branch

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,45 +3,56 @@ import crypto from "crypto";
 import { instance } from "../server.js";
 import { User } from "../schema.js";
 export const checkout = async (req, res) => {
+  try {
     const options = {
       amount: Number(req.body.amount * 100),
       currency: "INR",
-    receipt: `R-ABCS+${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`,
-  };
+      receipt: `R-ABCS+${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`,
+    };
 
-  const order = await instance.orders.create(options);
+    const order = await instance.orders.create(options);
 
-  await res.status(200).json({
-    success: true,
-    order,
-  });
+    await res.status(200).json({
+      success: true,
+      order,
+    });
+  }
+  catch (e) {
+    res.status(400).json({
+      success: false,
+      message: e.message,
+    });
+
+
+
+  }
 };
 
 export const paymentVerification = async (req, res) => {
 
-  try{
+  try {
 
     const {
       razorpay_order_id,
       razorpay_payment_id,
-    razorpay_signature,
-  } = req.body;
-    
-  const user = await User.findOne({ email: req.params.id });
-  
-  const sign = razorpay_order_id + "|" + razorpay_payment_id;
-  
-  const expectedSign = crypto
-  .createHmac("sha256", process.env.RZP_KEY_SECRET)
-  .update(sign.toString())
-  .digest("hex");
-  
-  // check both signs
-  const isAuthentic = expectedSign === razorpay_signature;
+      razorpay_signature,
+    } = req.body;
 
-  if (isAuthentic) {
-    // Database comes here
-    // await Payment.create({
+    const user = await User.findOne({ email: req.params.id });
+
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSign = crypto
+      .createHmac("sha256", process.env.RZP_KEY_SECRET)
+      .update(sign.toString())
+      .digest("hex");
+
+    // check both signs
+    const isAuthentic = expectedSign === razorpay_signature;
+
+    if (isAuthentic) {
+      // Database comes here
+      // await Payment.create({
       //   razorpay_order_id,
       //   razorpay_payment_id,
       //   razorpay_signature,
@@ -51,7 +62,7 @@ export const paymentVerification = async (req, res) => {
       res.redirect(
         `http://localhost:5173/paymentsuccess`
       );
-    } else{
+    } else {
       res.redirect("http://localhost:5173/paymentfailed")
     }
   } catch (e) {
