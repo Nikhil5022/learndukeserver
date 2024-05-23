af2db4072701b4ce39476ea1733ca9574c403af8
Gubbala Sai Nikhil <gsn.agc5022@gmail.com>
2024-05-23 11:45:12 +0530

    final changes at last

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://learndukeserver.vercel.app/auth/google/callback"
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
